1e106495d1333fb48d9969495ebb6182021b616d
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-03-09 15:18:17 +0530

    Add authentication to auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -6,3 +6,26 @@ import {initializeApp} from "./app.js";
 const provider = new GoogleAuthProvider();
 export const securityContext = {loggedUser: null};
 
+/* A listener to detect user status */
+onAuthStateChanged(auth, async (user) => {
+    if (user) {     /* Sign in */
+        securityContext.loggedUser = user.email;
+        $("#login, #splash").addClass("d-none");
+        $("#app").removeClass("d-none");
+        await initializeApp();
+    } else {        /* Sign out */
+        securityContext.loggedUser = null;
+        setTimeout(() => {
+            $("#login").removeClass("d-none");
+            $("#app, #splash").addClass("d-none");
+        }, 1000);
+    }
+});
+
+$("#btn-sign-in-google").on('click', async () => {
+    await signInWithPopup(auth, provider);
+});
+
+$("#btn-sign-out").on('click', async () => {
+    await signOut(auth);
+});
