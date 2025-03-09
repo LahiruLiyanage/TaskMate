fc09eb2a1679719c1ca2d2f8cf704ce312e750ed
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-03-09 14:01:18 +0530

    render task function implemented

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,40 @@
 import $ from 'jquery';
 
+export async function initializeApp() {
+    renderTasks();
+}
+
+function renderTasks() {
+    const noTask = $("#no-task");
+    const noCompletedTask = $("#no-completed-task");
+    const completedTaskList = $("#completed-task-list");
+
+    (taskList.find(task => !task.status)) ? noTask.hide() : noTask.show();
+    (taskList.find(task => task.status)) ? noCompletedTask.hide() : noCompletedTask.show();
+    (taskList.length) ? completedTaskList.show() : completedTaskList.hide();
+    $("#task-list > section, #completed-task-list > section").empty();
+
+    for (const {id, description, status} of taskList) {
+        const task = `
+            <div class="task-item d-flex justify-content-between p-2 align-items-center rounded-2 text-secondary-emphasis">
+              <div class="form-check">
+                <input  class="form-check-input" type="checkbox" 
+                        value="" id="${id}"
+                        ${status ? 'checked' : ''}>
+                <label  class="form-check-label" for="${id}">
+                        ${description}
+                </label>
+              </div>
+              <div class="d-flex gap-3 fs-5">
+                <i class="bi bi-pencil" title="Edit"></i>
+                <i class="bi bi-trash" title="Delete"></i>
+              </div>
+            </div>        
+        `;
+        $(!status ? "#task-list > section" : "#completed-task-list > section").prepend(task);
+    }
+}
+
 $('#chk-mode').on('change', function() {
     const darkMode = $(this).prop('checked');
 
